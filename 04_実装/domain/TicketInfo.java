b4d67250b4fcd7cbb7688b1839cf502b2ea89973
Reiko Miyashita <r-miyashita@cresco.co.jp>
2020-06-05 11:52:20 +0900

    変更追加

--- 04_実装/domain/TicketInfo.java
+++ 04_実装/domain/TicketInfo.java
@@ -7,7 +7,7 @@ public class TicketInfo {
 	
 	public TicketInfo() {
 		Ticket t1 = new Ticket("001", "演劇　  ", 1000,  "2020-07-07", 100);
-		Ticket t2 = new Ticket("002", "演奏会 ", 2000,  "2020-06-23", 0);
+		Ticket t2 = new Ticket("002", "演奏会 ", 2000,  "2020-06-23", 50);
 		Ticket t3 = new Ticket("003", "セミナー", 1500,  "2020-10-28", 80);
 		
 		ticket.add(t1);
@@ -52,7 +52,6 @@ public class TicketInfo {
 	
 	public Ticket get_ticket(String ticket_num, int reser_num_pur) {
 		Ticket t = null;
-		
 		for (int i = 0; i < ticket.size(); i++) {
 			t = ticket.get(i);
 			String t_num = t.get_ticket_num();
@@ -61,7 +60,6 @@ public class TicketInfo {
 			}
 		}
 		t.decrease_ticket_storage(reser_num_pur);
-		//System.out.println(t.get_ticket_storage());
 		return t;
 	}
 }
